d79f881f0637a32d6a07bd36d02e337ff438b68f
MattWiper <mattwiper@gmail.com>
2015-11-04 11:44:08 -0800

    Screen work. Edited run configs. Hello World

--- core/src/com/mwiper/game/screens/MainMenu.java
+++ core/src/com/mwiper/game/screens/MainMenu.java
@@ -1,15 +1,33 @@
 package com.mwiper.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by Matt on 11/4/2015.
  */
 public class MainMenu implements Screen{
 
+    SpriteBatch batch;
+    BitmapFont font;
+    public MainMenu(){
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.RED);
+    }
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.begin();
+        font.draw(batch, "Hello World", 200, 200);
+        batch.end();
     }
 
     @Override
@@ -39,6 +57,7 @@ public class MainMenu implements Screen{
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        font.dispose();
     }
 }
