9b56af991f5043394fbeeb2d783fe87b435e7358
MattWiper <mattwiper@gmail.com>
2015-11-04 10:12:12 -0800

    Initial structure work.

---
+++ core/src/com/mwiper/game/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.mwiper.game.utils;
+
+/**
+ * Created by Matt on 11/4/2015.
+ */
+public class Constants {
+    public static final int SCREEN_WIDTH = 600;
+    public static final int SCREEN_HEIGHT = 400;
+}
