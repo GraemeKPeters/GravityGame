29776b3df3e3ec6eeca3bdb8a17a271c03cfcc2d
MattWiper <mattwiper@gmail.com>
2015-11-07 11:29:30 -0800

    Added rendering to mainmenu

--- core/src/com/mwiper/game/screens/MainMenu.java
+++ core/src/com/mwiper/game/screens/MainMenu.java
@@ -4,11 +4,9 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -21,12 +19,10 @@ import com.mwiper.game.utils.Constants;
  * Created by Matt on 11/4/2015.
  */
 public class MainMenu implements Screen{
-
-    SpriteBatch batch;
-    BitmapFont font;
     Game game;
     private Viewport viewport;
     private Stage stage;
+
     public MainMenu(Game game){
         this.game = game;
         viewport = new FitViewport(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, new OrthographicCamera());
@@ -38,18 +34,23 @@ public class MainMenu implements Screen{
         table.center();
         table.setFillParent(true);
 
-        Label gameOverLabel = new Label("GAME OVER", font);
-        Label playAgainLabel = new Label("Click to Play Again", font);
+        Label gameLabel = new Label("Gravity", font);
+        Label playLabel = new Label("Click to Play!", font);
 
-        table.add(gameOverLabel).expandX();
+        table.add(gameLabel).expandX();
         table.row();
-        table.add(playAgainLabel).expandX().padTop(10f);
+        table.add(playLabel).expandX().padTop(10f);
 
         stage.addActor(table);
     }
     @Override
     public void render(float delta) {
+        if(Gdx.input.justTouched()) {
 
+        }
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
     }
 
     @Override
@@ -79,7 +80,6 @@ public class MainMenu implements Screen{
 
     @Override
     public void dispose() {
-        batch.dispose();
-        font.dispose();
+
     }
 }
