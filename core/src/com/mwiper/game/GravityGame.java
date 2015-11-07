76e83db40c4be8bd2b3860bb97c62e44f16a1da7
MattWiper <mattwiper@gmail.com>
2015-11-07 11:17:34 -0800

    Work on mainmenu. Refactored to libgdx standard

--- core/src/com/mwiper/game/GravityGame.java
+++ core/src/com/mwiper/game/GravityGame.java
@@ -1,13 +1,22 @@
 package com.mwiper.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mwiper.game.screens.MainMenu;
 
 public class GravityGame extends Game {
-	
+
+	public SpriteBatch batch;
 	@Override
 	public void create () {
-		setScreen(new MainMenu());
+		batch = new SpriteBatch();
+		setScreen(new MainMenu(this));
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		batch.dispose();
 	}
 
 }
--- core/src/com/mwiper/game/screens/MainMenu.java
+++ core/src/com/mwiper/game/screens/MainMenu.java
@@ -1,12 +1,21 @@
 package com.mwiper.game.screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mwiper.game.GravityGame;
+import com.mwiper.game.utils.Constants;
 
 /**
  * Created by Matt on 11/4/2015.
@@ -15,19 +24,32 @@ public class MainMenu implements Screen{
 
     SpriteBatch batch;
     BitmapFont font;
-    public MainMenu(){
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
+    Game game;
+    private Viewport viewport;
+    private Stage stage;
+    public MainMenu(Game game){
+        this.game = game;
+        viewport = new FitViewport(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, ((GravityGame) game).batch);
+
+        Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+
+        Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+
+        Label gameOverLabel = new Label("GAME OVER", font);
+        Label playAgainLabel = new Label("Click to Play Again", font);
+
+        table.add(gameOverLabel).expandX();
+        table.row();
+        table.add(playAgainLabel).expandX().padTop(10f);
+
+        stage.addActor(table);
     }
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
     }
 
     @Override
