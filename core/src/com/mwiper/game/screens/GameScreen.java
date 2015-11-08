e73d15bcfbf59e5f14f198c58f6a4caae50ea0aa
MattWiper <mattwiper@gmail.com>
2015-11-07 23:13:13 -0800

    Basic rendering on game screen added

--- core/src/com/mwiper/game/screens/GameScreen.java
+++ core/src/com/mwiper/game/screens/GameScreen.java
@@ -5,16 +5,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mwiper.game.GravityGame;
 import com.mwiper.game.utils.Constants;
+import com.mwiper.game.utils.WorldUtils;
 
 /**
  * Created by Matt on 11/4/2015.
  */
 public class GameScreen implements Screen {
+
+    private World world;
+    private Body ground;
+    private Box2DDebugRenderer renderer;
 
     private GravityGame game;
 
@@ -24,23 +32,26 @@ public class GameScreen implements Screen {
     public GameScreen(GravityGame game) {
         this.game = game;
 
-        gameCamera = new OrthographicCamera();
+        world = WorldUtils.createWorld();
+        ground = WorldUtils.createGround(world);
 
-        gamePort = new FitViewport(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, gameCamera);
+        renderer = new Box2DDebugRenderer();
 
-
-
-
+        setupCamera();
     }
-
+    private void setupCamera(){
+        gameCamera = new OrthographicCamera(20, 13);
+        gameCamera.position.set(gameCamera.viewportWidth / 2, gameCamera.viewportHeight / 2, 0f);
+        gameCamera.update();
+    }
     @Override
     public void render(float delta) {
-
-        //update(delta);
 
         //Clear the game screen with Black
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render(world, gameCamera.combined);
     }
 
     @Override
--- core/src/com/mwiper/game/utils/Constants.java
+++ core/src/com/mwiper/game/utils/Constants.java
@@ -1,9 +1,20 @@
 package com.mwiper.game.utils;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by Matt on 11/4/2015.
  */
 public class Constants {
     public static final int SCREEN_WIDTH = 600;
     public static final int SCREEN_HEIGHT = 400;
+
+    public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
+
+    public static final float GROUND_X = 0f;
+    public static final float GROUND_Y = 0f;
+    public static final float GROUND_WIDTH = 50f;
+    public static final float GROUND_HEIGHT = 2f;
+    public static final float GROUND_DENSITY = 0f;
+
 }
